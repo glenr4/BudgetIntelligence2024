dc0cf455176ea4e636a3c1ae485061ce450c1baf
Glen Rutherford <glenrutherford4@gmail.com>
2024-02-13 18:38:15 +1100

    Tidy up

--- BudgetIntelligence2024.Application/TransactionParserNAB.cs
+++ BudgetIntelligence2024.Application/TransactionParserNAB.cs
@@ -1,16 +1,7 @@
-﻿using MapsterMapper;
-
-namespace BudgetIntelligence2024.Application;
+﻿namespace BudgetIntelligence2024.Application;
 
 public class TransactionParserNAB : ITransactionParser
 {
-    /// <summary>
-    /// Imports transactions from a CSV file and inserts it into the TransactionStaging table
-    /// Use of this method must be inside a try, catch statement
-    /// </summary>
-    /// <param name="fileName">The CSV filename</param>
-    /// <returns>Status message</returns>
-    ///
     //TODO: data validation, how do we know if we have received garbage? Maybe the wrong file was selected?
     public IEnumerable<ImportedTransactionDto> ParseCSV(Stream file, int userId, int accountId)
     {
@@ -20,7 +11,6 @@ public class TransactionParserNAB : ITransactionParser
 
         //Logger.Debug(String.Format("ImportCSV: Importing {0} for userId: {1}, accountId: {2}", fileName, userId, accountId));
 
-        // Process file line by line
         try
         {
             using (StreamReader sr = new StreamReader(file))
@@ -28,12 +18,7 @@ public class TransactionParserNAB : ITransactionParser
                 string[] data;
                 while ((line = sr.ReadLine()) != null)
                 {
-                    // Replace ",," with "," "," so that the split always creates the same number of array elements
-                    //line = line.Replace(@""",,""", @""",""Empty"",""");
-                    //line = line.Replace(@""",,,""", @""",""Empty"",""Empty"",""");
-
-                    // Split into comma separated components
-                    string[] separator = new string[] { "," };   //{ @""",""" }; //, @""",,""" }; // ","
+                    string[] separator = new string[] { "," };
                     data = line.Split(separator, StringSplitOptions.None);
 
                     if (data.Length != 9)
@@ -43,7 +28,6 @@ public class TransactionParserNAB : ITransactionParser
                         continue;
                     }
 
-                    // Ignore first line which contains headings
                     if (data[0] == "Date")
                     {
                         LogFormatError(line);
@@ -70,19 +54,6 @@ public class TransactionParserNAB : ITransactionParser
             }
 
             return transactions;
-
-            // Save
-            //this.Context.SaveChanges();
-
-            //// Execute stored procedure to copy distinct entries added above to the staging table to the actual Transaction table
-            ////TODO: change staging table to a temporary table created by the stored procedure to avoid contention.
-            ////TODO: how do we avoid contention on the Transaction table, should there be one per user?
-            //ObjectParameter rowCount = new ObjectParameter("RowCount", typeof(int));
-            //this.Context.AddDistinctToTransactions(rowCount);   // TODO: need to prefix this stored procedure with "sp_"
-
-            //Logger.Debug(String.Format("ImportCSV: contains {0} transactions, {1} were new", lineCount, Convert.ToString(rowCount.Value)));
-
-            //return Convert.ToInt32(rowCount.Value);
         }
         catch (Exception ex)
         {
