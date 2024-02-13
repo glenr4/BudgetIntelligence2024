2c7256f743875ef32f2122839b29311c28679bef
Glen Rutherford <glenrutherford4@gmail.com>
2024-02-13 14:17:01 +1100

    Added ping endpoint

--- BudgetIntelligence2024.API/Program.cs
+++ BudgetIntelligence2024.API/Program.cs
@@ -1,10 +1,7 @@
 using BudgetIntelligence2024.API.Auth;
 using BudgetIntelligence2024.Application;
 using BudgetIntelligence2024.Persistence.DependencyInjection;
-using BudgetIntelligence2024.Persistence.StoredProcedures;
 using FastEndpoints;
-using Microsoft.AspNetCore.Mvc;
-using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateSlimBuilder(args);
 
@@ -16,6 +13,8 @@ builder.Services.AddPersistence();
 var app = builder.Build();
 
 app.UseFastEndpoints();
+
+app.MapGet("/ping", () => "pong");
 
 app.Run();
 
