0c44448891727372e95d2cef524d15aa914210e1
Glen Rutherford <glenrutherford4@gmail.com>
2024-02-15 10:14:15 +1100

    Added Serilog

--- BudgetIntelligence2024.API/Program.cs
+++ BudgetIntelligence2024.API/Program.cs
@@ -3,21 +3,47 @@ using BudgetIntelligence2024.Application;
 using BudgetIntelligence2024.Persistence.DependencyInjection;
 using FastEndpoints;
 using Mapster;
+using Serilog;
 
 var builder = WebApplication.CreateSlimBuilder(args);
 
-builder.Services.AddFastEndpoints();
+AddSerilog(builder);
 
-builder.Services.AddScoped<IUserContext, UserContext>();
-builder.Services.AddPersistence();
-builder.Services.AddApplication();
-builder.Services.AddMapster();
+AddServices(builder);
 
 var app = builder.Build();
 
-app.UseFastEndpoints();
-
-app.MapGet("/ping", () => "pong");
+AddEndpoints(app);
 
 app.Run();
 
+void AddSerilog(WebApplicationBuilder builder)
+{
+    using var log = 
+        new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Console()
+            .WriteTo.File("logs/BudgetIntelligence2024.log", rollingInterval: RollingInterval.Day)
+            .CreateLogger();
+    
+    Log.Logger = log;
+
+    builder.Host.UseSerilog();
+}
+
+void AddServices(WebApplicationBuilder builder)
+{
+    builder.Services.AddFastEndpoints();
+
+    builder.Services.AddScoped<IUserContext, UserContext>();
+    builder.Services.AddPersistence();
+    builder.Services.AddApplication();
+    builder.Services.AddMapster();
+}
+
+void AddEndpoints(WebApplication app)
+{
+    app.UseFastEndpoints();
+
+    app.MapGet("/ping", () => "pong");
+}
